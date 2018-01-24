0fbb34a85e688c8326dc79f93a79ea25eafc3f1d
Kento Komaki <k.knt49@gmail.com>
2018-01-24 11:00:06 +0000

    ログイン機能追加!

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,7 @@
 class BlogsController < ApplicationController
+  before_action :redirect, only: [:new, :edit, :show]
   before_action :set_blog, only: [:show, :edit, :update, :destroy]
-  
+
   def index
     @blogs = Blog.all
   end
@@ -41,6 +42,23 @@ class BlogsController < ApplicationController
       @blog = Blog.new(blog_params)
     else
       @blog = Blog.new
+    end
+  end
+  
+  
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+  
+  
+  def logged_in?
+    !current_user.nil?
+  end
+  
+  
+  def redirect
+    unless logged_in?
+      redirect_to new_session_path
     end
   end
   
