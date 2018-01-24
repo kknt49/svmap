e012109b7be5f9cc46f7179ec085af5bc2acae29
Kento Komaki <k.knt49@gmail.com>
2018-01-24 12:40:32 +0000

    トップ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   
+  get 'tops/index'
+  root to: "tops#index"
+  
   resources :blogs do
     collection do
       post :confirm
     end
   end
   
-  resources :contacts
-
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
 
