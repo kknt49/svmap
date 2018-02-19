e3de286f20dfca0c1cf20ee2bdd691e518b2f229
Kento Komaki <k.knt49@gmail.com>
2018-02-19 11:19:52 +0000

    awsにした4

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,12 +4,12 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :fog 
-  # storage :file     
+  # storage :fog 
+   storage :file     
   
   #if Rails.env == 'production'
   #  storage :fog  
-  #else  
+  #else   
   #  storage :file  
   #end
   
