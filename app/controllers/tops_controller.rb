fd41b0fc42085d7ee0a5ebcb297d9fc35494268b
Kento Komaki <k.knt49@gmail.com>
2018-02-22 08:27:34 +0000

    地図表示修正

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -2,27 +2,11 @@ class TopsController < ApplicationController
   def index
     @pictures = Picture.all
 
-#    gon.data = @pictures
-    
-#    @pictures_j = @pictures.to_json.html_safe
-    
-#    for i in 0..@pictures.length - 1 do
-#      gon.cate[i] = @pictures[i].cate
-#      gon.lat[i] = @pictures[i].lat
-#      gon.lon[i] = @pictures[i].lon
-#      gon.image[i] = @pictures[i].image_url
-#      gon.id[i] = @pictures[i].id
-#    end
-    
-
-    
     @cate = []
     @lat = []
     @lon = []
     @image = []
     @id = []
-
-
 
     for i in 0..@pictures.length - 1 do
       @lat[i] = @pictures[i].lat
@@ -40,6 +24,5 @@ class TopsController < ApplicationController
       @image_j = @image.to_json.html_safe
 
   end
-
 
 end
--- app/models/mapdatum.rb
+++
@@ -1,2 +0,0 @@
-class Mapdatum < ApplicationRecord
-end
