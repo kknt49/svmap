4638b67f4778018591793ece363817d2c10ecdec
Kento Komaki <k.knt49@gmail.com>
2018-02-22 11:00:42 +0000

    AWS対応

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,16 +103,16 @@ Rails.application.configure do
    enable_starttls_auto: true
   }
 
-#　 CarrierWave.configure do |config|  
-#    config.fog_credentials = {  
-#      provider: 'AWS',  
-#      aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],  
-#      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],  
-#      region: 'ap-northeast-1'  
-#    } 
-#    config.fog_directory = ENV['S3_BUCKET_NAME']  
-#    config.asset_host = "https://s3-ap-northeast-1.amazonaws.com/#{ ENV['S3_BUCKET_NAME'] }"  
-#    config.cache_storage = :fog
-#  end
+　 CarrierWave.configure do |config|  
+    config.fog_credentials = {  
+      provider: 'AWS',  
+      aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],  
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],  
+      region: 'ap-northeast-1'  
+    } 
+    config.fog_directory = ENV['S3_BUCKET_NAME']  
+    config.asset_host = "https://s3-ap-northeast-1.amazonaws.com/#{ ENV['S3_BUCKET_NAME'] }"  
+    config.cache_storage = :fog
+  end
 
 end
